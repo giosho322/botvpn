32d173705bd1cbf9ece010557ef90f52fa02208f
giosho322 <quesalno@outlook.com>
2025-06-23 18:35:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # Токен бота (бери у @BotFather, тупой)
-TOKEN = "тут_твой_токен_блять"  
+TOKEN = "AHHt-qKpuSZ23tC9_IB9LnrThaxH-W1EW0"  
 
 # Сюда впиши свой ID (узнаёшь у @userinfobot, дебил)
-ADMINS = [123456789]  
+ADMINS = [1203425573]  
 
 # IP твоего сервера (где WireGuard стоит, ну ты ж помнишь?)
-WG_SERVER_IP = "123.123.123.123"  
+WG_SERVER_IP = "80.74.28.21"  
 
 # Публичный ключ сервера (смотри в веб-морде WG или в конфигах)
-WG_SERVER_PUBKEY = "тут_ключ_сервера_сука"  
+WG_SERVER_PUBKEY = "zWriz05AYmvN5rOgop1jYLiR5ncyT3a/K5Yaoz+iiQw="  
 
 # API ключ от CryptoBot (если будешь через крипту брать)
 CRYPTOBOT_TOKEN = "твой_api_криптобота_сюда"  
